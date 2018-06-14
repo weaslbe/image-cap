da510c1c5acda5f972db5364fc4601417e68af61
Tobias Nack <tobiasnack13@googlemail.com>
2018-06-15 01:09:11 +0200

    Add minimal working example of Image captioning model via keras

--- quick_model.py
+++ quick_model.py
@@ -1,13 +1,14 @@
-from keras.layers import Conv2D, Input, Concatenate, Dense, Dropout, TimeDistributed, RepeatVector, Embedding, LSTM
+from keras.layers import Conv2D, Input, Concatenate, Dense, Dropout, TimeDistributed, RepeatVector, Embedding, Flatten, LSTM
 from keras.models import Model
+
 
 class QuickImageCaptioningModel:
 
     def build_image_model(self, coco_image):
         conv_blocks = []
-        conv_1 = Conv2D(100, (3, 3), activation='relu', data_format='channel_last')(coco_image)
-        conv_2 = Conv2D(100, (5, 5), activation='relu', data_format='channel_last')(coco_image)
-        conv_3 = Conv2D(100, (7, 7), activation='relu', data_format='channel_last')(coco_image)
+        conv_1 = Conv2D(5, (3, 3), activation='relu', data_format='channels_last')(coco_image)
+        conv_2 = Conv2D(5, (5, 5), activation='relu', data_format='channels_last')(coco_image)
+        conv_3 = Conv2D(5, (7, 7), activation='relu', data_format='channels_last')(coco_image)
         conv_1 = Flatten()(conv_1)
         conv_2 = Flatten()(conv_2)
         conv_3 = Flatten()(conv_3)
@@ -23,7 +24,7 @@ class QuickImageCaptioningModel:
 
     def language_model(self, seq_length,
                        dict_length, prev_words):
-        conv_feat = Input(shape=(1024))
+        conv_feat = Input(shape=(1024,))
 
         conv_repeat = RepeatVector(seq_length)(conv_feat)
 
@@ -31,9 +32,9 @@ class QuickImageCaptioningModel:
 
         lstm_in = Concatenate()([conv_repeat, emb])
 
-        lstm = LSTM(128, return_sequence=True)(lstm_in)
+        lstm = LSTM(128, return_sequences=True)(lstm_in)
 
-        predictions = TimeDistributed(Dense(dict_length,
+        predictions = TimeDistributed(Dense(dict_length + 1,
                                             activation='softmax'),
                                       name='out')(lstm)
 
@@ -44,13 +45,13 @@ class QuickImageCaptioningModel:
 
     def build_model(self, dict_length):
         seqlen = 20
-        coco_image = Input(shape=(400, 600))
+        coco_image = Input(shape=(128, 128, 3))
 
-        img_emb = self.build_image_model(coco_image)
+        img_emb = self.build_image_model(coco_image)(coco_image)
 
-        prev_words = Input(shape=(seqlen), name='prev_words')
+        prev_words = Input(shape=(seqlen,), name='prev_words')
 
-        lang_model = self.lang_model(seqlen, dict_length, prev_words)
+        lang_model = self.language_model(seqlen, dict_length, prev_words)
 
         out = lang_model([img_emb, prev_words])
 
--- quick_train.py
+++ quick_train.py
@@ -1,14 +1,73 @@
 from quick_model import QuickImageCaptioningModel
+from skimage import io, transform
+from keras.preprocessing.text import Tokenizer
+import json
+import numpy as np
+
+
+json_file = '/data/dl_lecture_data/TrainVal/annotations/captions_train2014.json'
+
 
 def load_train_data():
-    pass
+    json_captions = {}
+    with open(json_file, 'r') as f:
+        json_captions = json.load(f)
+    images = {}
+    data_set_images = []
+    data_set_annotation = []
+    data_set_output = []
+    relevant_captions = []
+    counter = 0
+    for i in json_captions['images']:
+        temp_image = io.imread('/data/dl_lecture_data/TrainVal/train2014/' + i['file_name'])
+        temp_image = np.array(transform.resize(temp_image, (128, 128)))
+        if temp_image.shape != (128, 128, 3):
+            continue
+        images[int(i['id'])] = temp_image
+        counter += 1
+        if counter > 100:
+            break
+
+    for i in json_captions['annotations']:
+        if int(i['image_id']) in images:
+            relevant_captions.append(i['caption'])
+            data_set_images.append(images[int(i['image_id'])])
+
+    print(data_set_images[0].shape)
+    print(len(relevant_captions))
+
+    tokenizer = Tokenizer(num_words=1024)
+
+    tokenizer.fit_on_texts(relevant_captions)
+
+    text_sequences = tokenizer.texts_to_sequences(relevant_captions)
+
+    for text_sequence in text_sequences:
+        if len(text_sequence) > 20:
+            text_sequence = text_sequence[:20]
+        while len(text_sequence) < 20:
+            text_sequence.append(0)
+        data_set_annotation.append(text_sequence)
+        temp_output = text_sequence[1:]
+        temp_output.append(0)
+        one_hot_output = []
+        for ouput in temp_output:
+            temp_hot = [0 for i in range(1025)]
+            temp_hot[ouput] = 1
+            one_hot_output.append(temp_hot)
+        data_set_output.append(one_hot_output)
+
+    return data_set_images, data_set_annotation, data_set_output
+
 
 def main():
     short_train_data = load_train_data()
 
     model = QuickImageCaptioningModel().build_model(1024)
 
-    model.compile('adam', loss='categorical_crossentropy', sample_weight_mode='temporal')
+    model.compile('adam', loss='categorical_crossentropy', sample_weight_mode='temporals')
+
+    model.fit([short_train_data[0], short_train_data[1]], np.array(short_train_data[2]), batch_size=16)
 
 
 if __name__ == "__main__":
